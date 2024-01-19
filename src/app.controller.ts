6ba187ffa20b706fbf93e38e001679e56223c9d0
temirhanb <temirhan023@gmail.com>
2024-01-19 21:09:42 +0500

    - create new models

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get} from "@nestjs/common";
+import {AppService} from "./app.service";
+import {DbService} from "./db/db.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private dbService: DbService) {
+  }
 
   @Get()
-  getHello(): string {
+  async getHello(): Promise<string> {
+    const user = this.dbService.user.findMany({});
+
     return this.appService.getHello();
   }
 }
